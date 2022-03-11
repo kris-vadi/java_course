f0fbefbc9c384c00661daee35152cce7f1d518fa
ЛÐÐKristina Vadi <you@christina.vadi@gmail.com>
2022-03-11 18:08:34 +0600

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,14 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car1 = new Car();
 
+        car1.setModel("Highlander");
+        car1.setColor("White");
+        car1.setYear(2016);
+
+        System.out.println("Модель автомобиля: " + car1.getModel());
+        System.out.println("Цвет автомобиля: " + car1.getColor());
+        System.out.println("Год выпуска автомобиля: " + car1.getYear());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,11 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike moto = new Motorbike("Honda", "Red", 2006);
+
+        System.out.println("Модель матоцикла: " + moto.getModel());
+        System.out.println("Цвет матоцикла: " + moto.getColor());
+        System.out.println("Год выпуска матоцикла: " + moto.getYear());
 
     }
 }
